df11083eaf5bf049199649e14d9a64b102414599
Sourav Mondal <sourav.mondal@innofied.com>
2015-07-20 19:23:56 +0530

    working on Fb Reach data fetch

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require("express"),
 app = express(),
 cors = require('cors'),
 bodyParser = require('body-parser'),
+exec = require('child_process').exec,
 request = require('request');
 app.use(cors());
 
@@ -14,13 +15,26 @@ app.get("/test",function(req,res){
 	res.send("Test success");
 })
 app.post('/getFbFeed', function(req, res) {
+	console.log(req.body);
 // Sending request to fb api using the request modules of node, the fb feed url is coming from
 // the frontend as a request parameter.
-request(req.body.url, function (error, response, body) {
+if(req.body.method === 'post') {
+	exec('curl -X POST ' + req.body.url, function(error, stdout, stderr) {
+		if(stdout) {
+			res.send(stdout);
+		} else {
+			res.send({error: error || stderr});
+			
+		}
+	})
+}else {
+	request(req.body.url, function (error, response, body) {
    if (!error && response.statusCode == 200) {
        res.send(body); // Send the response of the requested url to the frontend.
    }
 })
+}
+
 })
 app.listen(process.env.PORT || 3000);
 
